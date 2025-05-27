e0eb6cbf5732c286770bea6121a9d38c36842f34
Benjamin Pons <benjamin.pons@laplateforme.io>
2025-05-27 13:49:29 +0200

    feat : add anthill2 fonctionnal

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,7 @@ void anthill1() {
         ant.changeRoom(anthill[0]);
     }
     
-    // Links nodes together
+    // Links nodes toget    her
     int matrix[4][4] = {{0, 1, 1, 0},
                         {1, 0, 0, 1},
                         {1, 0, 0, 1},
@@ -78,9 +78,69 @@ void anthill1() {
     }
 }
 
+void anthill2() {
+    const int ANTS_AMOUNT = 5;
+
+    // Creating Nodes in a vector
+    vector<shared_ptr<Node>> anthill;
+    for (int i = 0; i < 4; ++i) {
+        anthill.push_back(make_shared<Node>(i));
+    }
+
+
+    for (int i = 0; i < 3; ++i) {
+        anthill[i]->capacity = 1;
+    }
+    anthill[3]->capacity = ANTS_AMOUNT;
+
+    // Creating Ants in a vector
+    vector<Ants> ants;
+    for (int i = 1; i <= ANTS_AMOUNT; ++i) {
+        ants.push_back(Ants(i));
+        ants.back().changeRoom(anthill[0]);
+    }
+
+
+    int matrix[4][4] = {{0, 1, 0, 0},
+                        {1, 0, 1, 0},
+                        {0,1 , 0, 1},
+                        {0, 0, 1, 0}};
+
+    for (int n = 0; n < 4; n++) {
+        for(int m = 0; m < 4; m++) {
+            if (matrix[n][m] == 1) {
+                anthill[n]->addNeighbor(anthill[m]);
+            }
+        }
+    }
+
+    int step = 1;
+    while (!anthill[3]->isFull()) {
+        string steps = "";
+
+        for (Ants& ant : ants) {
+            if (ant.room->id == 3) continue; // Déjà arrivé
+
+            for (shared_ptr<Node> neighbor : anthill[ant.room->id]->neighbors) {
+                if (!neighbor->isFull() && neighbor->id > ant.room->id) {
+                    int old_room = ant.room->id;
+                    ant.changeRoom(neighbor);
+                    steps += "f" + to_string(ant.name) + " - S" + to_string(old_room) + " -> S" + to_string(ant.room->id) + "\n";
+                    break;
+                }
+            }
+        }
+        // Displays steps in terminal
+        printStepBar(step++);
+        cout << steps;
+        Sleep(1000);
+    }
+}
+
+
 int main() {
     
-    anthill1();
+    anthill2();
 
     return 0;
 }
