00da1b799a5afbd4768e0e3c1c79bdfe4b7d6be7
Benjamin Pons <benjamin.pons@laplateforme.io>
2025-05-28 09:05:12 +0200

    feat : add anthill 3

--- src/main.cpp
+++ src/main.cpp
@@ -136,6 +136,55 @@ void anthill2(){
     }
     algorithm(anthill, ants);
 }
+
+void anthill3(){
+        const int ANTS_AMOUNT = 5;
+
+    // Creating Nodes in a vector
+    std::vector<std::shared_ptr<Node>> anthill;
+    for (int i = 0; i < 4; i++) {
+        anthill.push_back(std::make_shared<Node>(i));;
+    }
+
+    anthill[3]->isEnding = true;
+
+    // Creating Ants in a vector
+    vector<Ants> ants;
+    for (int i = 1; i < ANTS_AMOUNT+1; i++) {
+        ants.push_back(Ants(i));
+    }
+
+    for (int i = 0; i < 3; ++i) {
+        anthill[i]->capacity = 1;
+    }
+
+    // Changing last node capacity to ants amount
+    anthill[3]->capacity = ANTS_AMOUNT;
+
+    // Putting ants in starting room
+    for (Ants& ant : ants) {
+        ant.changeRoom(anthill[0]);
+    }
+
+
+    int matrix[6][6] = {{0, 1, 0, 0, 0, 0},
+                        {1, 0, 1, 0, 1, 0},
+                        {0,1 , 0, 1, 0, 0},
+                        {0, 0, 1, 0, 0, 0},
+                        {0, 1, 0, 0, 0, 1},
+                        {0, 0, 0, 0, 1 ,0}};
+
+    for (int n = 0; n < 4; n++) {
+        for(int m = 0; m < 4; m++) {
+            if (matrix[n][m] == 1) {
+                anthill[n]->addNeighbor(anthill[m]);
+            }
+        }
+    }
+    algorithm(anthill, ants);
+}
+
+
 int main() {
     
     anthill2();
